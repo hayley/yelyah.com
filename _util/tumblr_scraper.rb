5eb08cfc21c2c7c334f18d79ae23f16b37e81d38
hayley <github@vortex.cx>
2011-02-01 10:04:52 -0600

    adds a readme of sorts to the comments of the scraper script

--- _util/tumblr_scraper.rb
+++ _util/tumblr_scraper.rb
@@ -1,8 +1,21 @@
 require 'rubygems'
 require 'httparty'
 
-# ||= for testing in case it's already been set previously in the session
-# only an issue since tumblr keeps dying
+# here's how I've retaught myself how this thing works
+# cd to a temp directory
+# mkdir _posts
+# irb
+# require 'httparty'
+## now set the posts manually
+# @posts = HTTParty.get('http://t.yelyah.com/api/read?filter=none&type=audio&num=50')['tumblr']['posts']['post']
+## now run the script to generate the files
+# load "/Users/hayley/frozen/yelyah.com/_util/tumblr_scraper.rb"
+## if there are more than 50 posts you need:
+# @posts = HTTParty.get('http://t.yelyah.com/api/read?filter=none&type=audio&num=50&start=50')['tumblr']['posts']['post']#
+# and load the script again
+# repeat the process for every block of 50 that you need
+
+# if you haven't already set @posts, this line will give you the 5 most recent
 @posts ||= HTTParty.get('http://t.yelyah.com/api/read?filter=none&type=audio&num=5')['tumblr']['posts']['post']
 
 @posts.each do |post|
